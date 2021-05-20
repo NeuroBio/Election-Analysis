fb3fd134ce62dc64c5eab58a15aa3094897c19ae
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-05-20 12:47:52 -0400

    coutn total votes

--- PyPoll.py
+++ PyPoll.py
@@ -4,16 +4,20 @@ import os
 # get data and create file for analysis
 input_path = os.path.join('Resources', 'election_results.csv')
 output_path = os.path.join('analysis', 'election_analysis.txt')
-analysis = open(output_path, "w")
+# analysis = open(output_path, "w")
 
 # Load in data
 with open(input_path) as election_file:
     election_data = csv.reader(election_file)
     next(election_data)
-    for row in election_data:
-        print(row)
 
-    # Get the number of votes cast from the length of the data.
+    num_votes = 0
+
+    for row in election_data:
+
+        # Get the number of votes cast from the length of the data.
+        num_votes += 1
+
         # analysis.write()
     # Get a set of the candidates
     # Create a data structure to hold the votes per candidate
@@ -23,4 +27,4 @@ with open(input_path) as election_file:
     # Use a one-time-pass method to get the candidate with the largest precentage
         #analysis.write()
 
-analysis.close()
+# analysis.close()
