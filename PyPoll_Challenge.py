c5a1ced18fc8d0a0d426f4b627d593d75b7ee329
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-05-20 18:31:21 -0400

    tasks 1-5

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -18,6 +18,8 @@ candidate_options = []
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
+counties = []
+county_votes = {}
 
 
 
@@ -27,7 +29,8 @@ winning_count = 0
 winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
-
+largest_turnout_county = ""
+largest_turout_votes = 0
 
 
 # Read the csv and convert it into a list of dictionaries
@@ -47,7 +50,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-
+        county_name = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -64,16 +67,16 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-
-
+        if county_name not in counties:
+ 
             # 4b: Add the existing county to the list of counties.
-
+            counties.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
-
+            county_votes[county_name] = 0
 
         # 5: Add a vote to that county's vote count.
-
+        county_votes[county_name] += 1
 
 
 # Save the results to our text file.
